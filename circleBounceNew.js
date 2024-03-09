2a122243a1240e463e85bb231244c8f79636662f
Jan Thiel <jmt28@st-andrews.ac.uk>
2024-03-09 17:42:16 +0100

    _

--- circleBounceNew.js
+++ circleBounceNew.js
@@ -3698,7 +3698,7 @@ function MaskRoutineBegin(snapshot) {
     Mended = false
     //rotationSpeed = (0.0035 * nm);
     var circleCount;
-    circleCount = cM;
+    circleCount = cMM;
     iterationS = 4;
     
     startDelay = ((radius / circleCount) / 2);
@@ -3751,7 +3751,7 @@ function MaskRoutineBegin(snapshot) {
     barfor.size = [(Progress*0.16 / 48), 0.08];
     //}
     Condition = 1;
-    console.log('l:', l, 's:', s, 'q:', q, 'cM:', cM )
+    console.log('l:', l, 's:', s, 'q:', q, 'cMM:', cMM )
     continueCondition = true;
     // setup some python lists for storing info about the mouse_5
     // current position of the mouse:
