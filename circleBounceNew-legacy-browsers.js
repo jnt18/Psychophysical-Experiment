ead74f03be31bfa5ebbd48beab2a97115a0d0f43
Jan Thiel <jmt28@st-andrews.ac.uk>
2024-03-13 19:08:57 +0100

    added thisExp.addData('c2 position:', circle2.pos) for all red dots

--- circleBounceNew-legacy-browsers.js
+++ circleBounceNew-legacy-browsers.js
@@ -422,7 +422,7 @@ async function experimentInit() {
     text: 'During the experiment, clicking on this screen\nwill immediatly cause moving dots to appear! ',
     font: 'Open Sans',
     units: undefined, 
-    pos: [0, ((- 0.25) * radius)], height: 0.02,  wrapWidth: undefined, ori: 0.0,
+    pos: [0, ((- 0.1) * radius)], height: 0.02,  wrapWidth: undefined, ori: 0.0,
     languageStyle: 'LTR',
     color: new util.Color('black'),  opacity: undefined,
     depth: -3.0 
@@ -1664,7 +1664,7 @@ function Line0RoutineEnd(snapshot) {
     l5.autoDraw = false;
     Line.autoDraw = false;
     circle0.autoDraw = false;
-    
+    thisExp.addData('c0 position:', circle0.pos)
     // store data for psychoJS.experiment (ExperimentHandler)
     if (mouse_15.x) {  psychoJS.experiment.addData('mouse_15.x', mouse_15.x[0])};
     if (mouse_15.y) {  psychoJS.experiment.addData('mouse_15.y', mouse_15.y[0])};
@@ -1823,6 +1823,7 @@ function Halo_01RoutineEnd(snapshot) {
     l5.autoDraw = false;
     circle0.autoDraw = false;
     circle1.autoDraw = false;
+    thisExp.addData('c1 position:', circle1.pos)
     // store data for psychoJS.experiment (ExperimentHandler)
     if (mouse_16.x) {  psychoJS.experiment.addData('mouse_16.x', mouse_16.x[0])};
     if (mouse_16.y) {  psychoJS.experiment.addData('mouse_16.y', mouse_16.y[0])};
@@ -1986,6 +1987,7 @@ function Halo_02RoutineEnd(snapshot) {
     circle0.autoDraw = false;
     circle1.autoDraw = false;
     circle2.autoDraw = false;
+    thisExp.addData('c2 position:', circle2.pos)
     // store data for psychoJS.experiment (ExperimentHandler)
     if (mouse_17.x) {  psychoJS.experiment.addData('mouse_17.x', mouse_17.x[0])};
     if (mouse_17.y) {  psychoJS.experiment.addData('mouse_17.y', mouse_17.y[0])};
@@ -2188,7 +2190,7 @@ function Halo_03RoutineEnd(snapshot) {
     circle2.autoDraw = false;
     circle3.autoDraw = false;
     averageCircle.autDraw = false
-    
+    thisExp.addData('c3 position:', circle3.pos)
     
     // store data for psychoJS.experiment (ExperimentHandler)
     if (mouse_18.x) {  psychoJS.experiment.addData('mouse_18.x', mouse_18.x[0])};
@@ -3079,6 +3081,7 @@ function LineRoutineEnd(snapshot) {
     l5.autoDraw = false;
     Line.autoDraw = false;
     circle0.autoDraw = false;
+    thisExp.addData('c0 position:', circle0.pos)
     // store data for psychoJS.experiment (ExperimentHandler)
     if (mouse_7.x) {  psychoJS.experiment.addData('mouse_7.x', mouse_7.x[0])};
     if (mouse_7.y) {  psychoJS.experiment.addData('mouse_7.y', mouse_7.y[0])};
@@ -3246,6 +3249,7 @@ function Halo_1RoutineEnd(snapshot) {
     l5.autoDraw = false;
     circle0.autoDraw = false;
     circle1.autoDraw = false;
+    thisExp.addData('c1 position:', circle1.pos)
     // store data for psychoJS.experiment (ExperimentHandler)
     if (mouse_2.x) {  psychoJS.experiment.addData('mouse_2.x', mouse_2.x[0])};
     if (mouse_2.y) {  psychoJS.experiment.addData('mouse_2.y', mouse_2.y[0])};
@@ -3414,6 +3418,7 @@ function Halo_2RoutineEnd(snapshot) {
     circle0.autoDraw = false;
     circle1.autoDraw = false;
     circle2.autoDraw = false;
+    thisExp.addData('c2 position:', circle2.pos)
     // store data for psychoJS.experiment (ExperimentHandler)
     if (mouse_3.x) {  psychoJS.experiment.addData('mouse_3.x', mouse_3.x[0])};
     if (mouse_3.y) {  psychoJS.experiment.addData('mouse_3.y', mouse_3.y[0])};
@@ -3608,6 +3613,7 @@ function Halo_3RoutineEnd(snapshot) {
     circle1.autoDraw = false;
     circle2.autoDraw = false;
     circle3.autoDraw = false;
+    thisExp.addData('c3 position:', circle3.pos)
     
     //for (var i, _pj_c = 0, _pj_a = util.range(c), _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
         //i = _pj_a[_pj_c];
